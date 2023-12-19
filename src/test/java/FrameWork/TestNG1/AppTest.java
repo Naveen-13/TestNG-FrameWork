8c2ab8f25c0a1a26d9a718b5a435665ae973b276
Naveen-13 <naveenbhat5341@gmail.com>
2023-12-20 01:10:41 +0530

    First commit

---
+++ src/test/java/FrameWork/TestNG1/AppTest.java
@@ -0,0 +1,12 @@
+package FrameWork.TestNG1;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Method1");
+    }
+}
